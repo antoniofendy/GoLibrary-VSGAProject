bce45282da1f710dc1ed4c8c640ec4171f4fda0b
antoniofendy <layfenfen@gmail.com>
2021-08-06 14:23:57 +0700

    Preparing administrator page

---
+++ admin/_include_admin/head.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<link rel="icon" href="../img/logo-sm-grey.png">
+<title>Administrator Dashboard</title>
+
+<!-- Custom fonts for this template-->
+<link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+<link rel="stylesheet" href="../css/admin/styles-admin.css" type="text/css">
+
+<!-- Custom styles for this template-->
+<link href="../css/admin/sb-admin-2.min.css" rel="stylesheet">
---
+++ admin/_include_admin/sidebar.php
@@ -0,0 +1,86 @@
+<style>
+  .sidebar {
+    background-color: #1e3799;
+  }
+</style>
+
+<ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar">
+
+  <!-- Sidebar - Brand -->
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+    <div class="sidebar-brand-icon">
+      <i class="fas fa-cogs"></i>
+    </div>
+    <div class="sidebar-brand-text mx-2 accent-font" style="text-transform:none;">Administrator Dashboard</div>
+  </a>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider my-0">
+
+  <!-- Nav Item - Dashboard -->
+  <li class="nav-item <?php echo $current_page == 'dashboard' ? 'active' : '' ?>">
+    <a class="nav-link" href="index.php">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span>Dashboard</span></a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider">
+
+  <!-- Heading -->
+  <div class="sidebar-heading accent-font">
+    Master Data
+  </div>
+
+  <?php if ($_SESSION['level'] == "1") { ?>
+    <!-- Nav Item - Users -->
+    <li class="nav-item <?php echo $current_page == 'users' ? 'active' : '' ?>">
+      <a href="users.php" class="nav-link">
+        <i class="fas fa-user-friends"></i>
+        <span>Users</span>
+      </a>
+    </li>
+
+  <?php } ?>
+
+  <!-- Nav Item - Messages -->
+  <li class="nav-item <?php echo $current_page == 'messages' ? 'active' : '' ?>">
+    <a href="messages.php" class="nav-link">
+      <i class="fas fa-envelope"></i>
+      <span>Messages</span>
+    </a>
+  </li>
+
+  <!-- Nav Item - Articles -->
+  <li class="nav-item <?php echo $current_page == 'articles' ? 'active' : '' ?>">
+    <a href="articles.php" class="nav-link">
+      <i class="fas fa-align-center"></i>
+      <span>Articles</span>
+    </a>
+  </li>
+
+  <!-- Nav Item - Locations -->
+  <li class="nav-item <?php echo $current_page == 'locations' ? 'active' : '' ?>">
+    <a href="locations.php" class="nav-link">
+      <i class="fas fa-map-marked-alt"></i>
+      <span>Locations</span>
+    </a>
+  </li>
+
+  <!-- Nav Item - Hotels -->
+  <li class="nav-item <?php echo $current_page == 'hotels' ? 'active' : '' ?>">
+    <a href="hotels.php" class="nav-link">
+      <i class="fas fa-hotel"></i>
+      <span>Hotels</span>
+    </a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+  <!-- Sidebar Toggler (Sidebar) -->
+  <div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+  </div>
+
+</ul>
